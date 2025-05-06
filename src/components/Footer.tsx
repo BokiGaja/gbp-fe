f944d1c61603ad09a8d93043cf689e66b4f734dc
nemanjagajic <nemanja.gajic@ananas.rs>
2025-05-06 20:22:10 +0200

    Add translations to Footer component for improved internationalization support

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useTranslations } from 'next-intl';
 
 const Footer = () => {
+  const t = useTranslations('footer');
+
   return (
     <footer className="bg-[#000D2D] text-[16px] font-sans pt-12 pb-6">
       <div className="mx-auto px-4 flex flex-col lg:flex-row justify-between items-start gap-12 lg:gap-0">
@@ -38,32 +41,64 @@ const Footer = () => {
         </div>
         {/* Product & Services */}
         <div className="min-w-[220px] md:mb-8">
-          <div className="text-white font-[500] text-2xl mb-6">Product & Services</div>
+          <div className="text-white font-[500] text-2xl mb-6">
+            {t('productServices')}
+          </div>
           <div className="flex flex-col gap-4">
-            <a href="#" className="text-white/50 no-underline">Vitez</a>
-            <a href="#" className="text-white/50 no-underline">Remont</a>
-            <a href="#" className="text-white/50 no-underline">Air</a>
-            <a href="#" className="text-white/50 no-underline">Sea</a>
-            <a href="#" className="text-white/50 no-underline">Ground</a>
-            <a href="#" className="text-white/50 no-underline">Consulting</a>
-            <a href="#" className="text-white/50 no-underline">Mediation</a>
+            <a href="#" className="text-white/50 no-underline">
+              {t('services.vitez')}
+            </a>
+            <a href="#" className="text-white/50 no-underline">
+              {t('services.remont')}
+            </a>
+            <a href="#" className="text-white/50 no-underline">
+              {t('services.air')}
+            </a>
+            <a href="#" className="text-white/50 no-underline">
+              {t('services.sea')}
+            </a>
+            <a href="#" className="text-white/50 no-underline">
+              {t('services.ground')}
+            </a>
+            <a href="#" className="text-white/50 no-underline">
+              {t('services.consulting')}
+            </a>
+            <a href="#" className="text-white/50 no-underline">
+              {t('services.mediation')}
+            </a>
           </div>
         </div>
         {/* Who we are */}
         <div className="min-w-[220px] md:mb-8">
-          <div className="text-white font-[500] text-2xl mb-6">Who we are</div>
+          <div className="text-white font-[500] text-2xl mb-6">
+            {t('whoWeAre')}
+          </div>
           <div className="flex flex-col gap-4">
-            <a href="#" className="text-white/50 no-underline">Events</a>
-            <a href="#" className="text-white/50 no-underline">Partners</a>
-            <a href="#" className="text-white/50 no-underline">Careers</a>
-            <a href="#" className="text-white/50 no-underline">Licenses</a>
-            <a href="#" className="text-white/50 no-underline">About Us</a>
-            <a href="#" className="text-white/50 no-underline">Join Team</a>
+            <a href="#" className="text-white/50 no-underline">
+              {t('links.events')}
+            </a>
+            <a href="#" className="text-white/50 no-underline">
+              {t('links.partners')}
+            </a>
+            <a href="#" className="text-white/50 no-underline">
+              {t('links.careers')}
+            </a>
+            <a href="#" className="text-white/50 no-underline">
+              {t('links.licenses')}
+            </a>
+            <a href="#" className="text-white/50 no-underline">
+              {t('links.aboutUs')}
+            </a>
+            <a href="#" className="text-white/50 no-underline">
+              {t('links.joinTeam')}
+            </a>
           </div>
         </div>
         {/* Our Info */}
         <div className="min-w-[320px] mb-8">
-          <div className="text-white font-[500] text-2xl mb-6">Our Info</div>
+          <div className="text-white font-[500] text-2xl mb-6">
+            {t('ourInfo')}
+          </div>
           <div className="text-white/50 mb-4">emailexample@gbp.com</div>
           <div className="text-white/50">Serbia, Lapovo, Ul.Ratka 43a</div>
         </div>
@@ -71,13 +106,25 @@ const Footer = () => {
       {/* Bottom bar */}
       <div className="md:mt-12 pt-6 mx-auto px-4 flex flex-col lg:flex-row lg:justify-between lg:items-center text-[16px]">
         <div className="flex flex-col md:flex-row gap-6 md:gap-10 flex-wrap mb-4 md:mb-0">
-          <a href="#" className="text-white/50 no-underline">Privacy policy</a>
-          <a href="#" className="text-white/50 no-underline">Terms of use</a>
-          <a href="#" className="text-white/50 no-underline">Accessibility</a>
-          <a href="#" className="text-white/50 no-underline">Modern Slavery Statement</a>
-          <a href="#" className="text-white/50 no-underline">Cookies Settings</a>
+          <a href="#" className="text-white/50 no-underline">
+            {t('legal.privacyPolicy')}
+          </a>
+          <a href="#" className="text-white/50 no-underline">
+            {t('legal.termsOfUse')}
+          </a>
+          <a href="#" className="text-white/50 no-underline">
+            {t('legal.accessibility')}
+          </a>
+          <a href="#" className="text-white/50 no-underline">
+            {t('legal.modernSlavery')}
+          </a>
+          <a href="#" className="text-white/50 no-underline">
+            {t('legal.cookiesSettings')}
+          </a>
         </div>
-        <div className="text-white/50 mt-8">© GPB 2025</div>
+        <div className="text-white/50 mt-8 lg:mt-0">
+          {t('copyright')}
+        </div>
       </div>
     </footer>
   );
