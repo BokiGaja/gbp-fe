6f13854bdc7ffcb7953e1666fdc3a752ec2056f0
nemanjagajic <nemanja.gajic@ananas.rs>
2025-09-28 15:44:14 +0200

    Fix optional chaining in buildCategoryPath function to handle undefined parents and slug, improving robustness of category path generation.

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -2,8 +2,8 @@ export const buildCategoryPath = (
   category: { slug: string; parents?: { slug: string }[] },
   lang: string
 ) => {
-  const slugs = category.parents
+  const slugs = category?.parents
     ? [...category.parents.map((p) => p.slug), category.slug]
-    : [category.slug];
+    : [category?.slug];
   return `/${lang}/categories/${slugs.join('/')}`;
 };
