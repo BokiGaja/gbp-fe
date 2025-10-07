03836a58757ba13dfba4047e67bb26cbcd51b5cf
nemanjagajic <nemanja.gajic@ananas.rs>
2025-10-07 15:29:31 +0200

    Refactor ItemPage component to render item description as HTML using dangerouslySetInnerHTML, enhancing content display and styling.

--- src/app/[lang]/items/[slug]/page.tsx
+++ src/app/[lang]/items/[slug]/page.tsx
@@ -6,7 +6,6 @@ import React from 'react';
 import { useItem } from '@/hooks/useCategories';
 import { useParams } from 'next/navigation';
 import Breadcrumbs from '@/components/Breadcrumbs';
-import MarkdownRenderer from '@/components/MarkdownRenderer';
 import ImageCarousel from '@/components/ImageCarousel';
 import VideoPreview from '@/components/VideoPreview';
 import NotFound from '@/components/NotFound';
@@ -47,10 +46,11 @@ const ItemPage = () => {
 
           {/* Item Content */}
           <div className="flex justify-center mb-[100px]">
-            {/* Description */}
-            <div className="max-w-2xl space-y-6">
-              {item.description && <MarkdownRenderer content={item.description} />}
-            </div>
+            {/* Description (HTML from item.description) */}
+            <div
+              className="max-w-2xl font-sans text-[#000D2D] [&>h3]:text-3xl [&>h3]:font-[500] [&>h3]:mb-6 [&>h4]:font-[500] [&>h4]:text-[#000D2D]/70 [&>p]:text-base [&>p]:leading-8 [&>p]:text-[#000D2D]/70 [&>p+*]:mt-6 [&_ul]:list-disc [&_ul]:pl-6 [&_ul]:mt-2 [&_ul]:mb-6 [&_ul]:text-base [&_ol]:list-decimal [&_ol]:pl-6 [&_ol]:mt-2 [&_ol]:mb-6 [&_ol]:text-base [&_li]:text-base [&_li]:leading-8 [&_li]:text-[#000D2D]/70 [&_a]:text-[#000D2D]/70 [&_strong]:text-[#000D2D]/70 [&_em]:text-[#000D2D]/70"
+              dangerouslySetInnerHTML={{ __html: item.description || '' }}
+            />
           </div>
 
           {/* Video Preview */}
