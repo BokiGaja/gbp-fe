2c2ac9d7ff08d5a2c0b02f6df6911aab233e730b
Nemanja <nemanja.gajicru96@gmail.com>
2025-11-21 16:09:18 +0100

    Refactor Footer component to update social media links and improve accessibility. Replace placeholder links with a LinkedIn link, and comment out unused social media sections for future reference. Add a note for legal links in the bottom bar.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -56,30 +56,35 @@ const Footer = () => {
             </Link>
           </div>
           <div className="flex flex-col gap-5">
-            <a href="#" className="flex items-center text-white/50 no-underline">
+            {/* <a href="#" className="flex items-center text-white/50 no-underline">
               <span className="bg-white rounded-full flex items-center justify-center mr-3">
                 <img src="/instagram.svg" alt="Instagram" className="h-6 w-6" />
               </span>
               Instagram
-            </a>
-            <a href="#" className="flex items-center text-white/50 no-underline">
+            </a> */}
+            {/* <a href="#" className="flex items-center text-white/50 no-underline">
               <span className="bg-white rounded-full flex items-center justify-center mr-3">
                 <img src="/twitter.svg" alt="Twitter" className="h-6 w-6" />
               </span>
               Twitter
-            </a>
-            <a href="#" className="flex items-center text-white/50 no-underline">
+            </a> */}
+            <a
+              href="https://www.linkedin.com/company/gbp-defence/?originalSubdomain=rs"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center text-white/50 no-underline hover:text-white transition-colors"
+            >
               <span className="bg-white rounded-full flex items-center justify-center mr-3">
                 <img src="/linkedin.svg" alt="LinkedIn" className="h-6 w-6" />
               </span>
               LinkedIn
             </a>
-            <a href="#" className="flex items-center text-white/50 no-underline">
+            {/* <a href="#" className="flex items-center text-white/50 no-underline">
               <span className="bg-white rounded-full flex items-center justify-center mr-3">
                 <img src="/youtube.svg" alt="Youtube" className="h-6 w-6" />
               </span>
               Youtube
-            </a>
+            </a> */}
           </div>
         </div>
         {/* Product & Services */}
@@ -157,8 +162,8 @@ const Footer = () => {
           <div className="text-white/50">Serbia, Lapovo, Ul.Ratka 43a</div>
         </div>
       </div>
-      {/* Bottom bar */}
-      <div className="md:mt-12 pt-6 mx-auto px-4 flex flex-col lg:flex-row lg:justify-between lg:items-center text-[16px]">
+      {/* Bottom bar - ToDO: Add legal links */}
+      {/* <div className="md:mt-12 pt-6 mx-auto px-4 flex flex-col lg:flex-row lg:justify-between lg:items-center text-[16px]">
         <div className="flex flex-col md:flex-row gap-6 md:gap-10 flex-wrap mb-4 md:mb-0">
           <a href="#" className="text-white/50 no-underline">
             {t('legal.privacyPolicy')}
@@ -177,7 +182,7 @@ const Footer = () => {
           </a>
         </div>
         <div className="text-white/50 mt-8 lg:mt-0">{t('copyright')}</div>
-      </div>
+      </div> */}
     </footer>
   );
 };
